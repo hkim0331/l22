483b053b1bf4222ea007dfc8653e9e39d5374e5f
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-03-16 21:51:28 +0900

    something bad happened

--- src/clj/l22/routes/home.clj
+++ src/clj/l22/routes/home.clj
@@ -7,6 +7,13 @@
    [ring.util.response]
    [ring.util.http-response :as response]))
 
+(defn register [request]
+  (layout/render [request] "register.html"))
+
+(defn register! [{:keys [params] :as request}]
+  (layout/render [request] "home.html" {:docs (str params)}))
+
+
 (defn home-page [request]
   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
@@ -14,9 +21,11 @@
   (layout/render request "about.html"))
 
 (defn home-routes []
-  [ ""
+  [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
+   ["/register" {:get  register
+                 :post register!}]
    ["/" {:get home-page}]
    ["/about" {:get about-page}]])
 
