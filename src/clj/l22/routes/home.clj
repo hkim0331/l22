69f680a69bcf70c76dbd4716a0f6b4231045c533
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2022-04-14 12:46:13 +0900

    under construction

--- src/clj/l22/routes/home.clj
+++ src/clj/l22/routes/home.clj
@@ -9,7 +9,7 @@
    [l22.password :refer [password password!]]
    [ring.util.response]))
 
-(def ^:private version "0.2.14")
+(def ^:private version "0.2.15-SNAPSHOT")
 ;; only works in development. not in jar.
 ;; (def ^:private version
 ;;   (-> "project.clj" slurp read-string (nth 2)))
