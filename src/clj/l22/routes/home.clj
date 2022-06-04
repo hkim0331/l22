eb190bae58d2b08664124004f7643f9dbf15e1d4
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-06-04 20:55:33 +0900

    0.5.0-SNAPSHOT

--- src/clj/l22/routes/home.clj
+++ src/clj/l22/routes/home.clj
@@ -9,8 +9,8 @@
    [l22.password :refer [password password!]]
    [ring.util.response]))
 
-(def ^:private version "0.4.5")
-(def ^:private updated_at "2022-05-29 13:25:08")
+(def ^:private version "0.5.0-SNAPSHOT")
+(def ^:private updated_at "2022-06-04 20:55:24")
 ;; below only works in development, not in jar.
 ;; (def ^:private version
 ;;   (-> "project.clj" slurp read-string (nth 2)))
