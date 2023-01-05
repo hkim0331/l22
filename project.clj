9b5b766c4762fff4230f71abfab8e99e82cdfaf8
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-01-05 16:40:35 +0900

    prep release 0.10.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject l22 "0.9.11"
+(defproject l22 "0.10.12"
 
   :description "for literacy 2022 classes"
   :url "https://l22.melt.kyutech.ac.jp"
--- src/clj/l22/routes/home.clj
+++ src/clj/l22/routes/home.clj
@@ -9,8 +9,8 @@
    [l22.password :refer [password password!]]
    [ring.util.response]))
 
-(def ^:private version "0.9.11")
-(def ^:private updated_at "2022-12-28 11:08:07")
+(def ^:private version "0.10.12")
+(def ^:private updated_at "2023-01-05 16:37:49")
 
 (defn home-page [{:keys [flash] :as request}]
   (layout/render request "home.html"
