3313387ed477dc5b11449f565804135ebd7784d0
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-06-28 09:48:01 +0900

    filter by ayear

--- src/clj/l22/routes/home.clj
+++ src/clj/l22/routes/home.clj
@@ -8,8 +8,8 @@
    [l22.profile :refer [profile-login profile-show]]
    [ring.util.response]))
 
-(def ^:private version "v2.5.597")
-(def ^:private updated_at "2024-06-27 22:56:14")
+(def ^:private version "v2.5.602")
+(def ^:private updated_at "2024-06-28 09:47:40")
 
 (defn home-page [{:keys [flash] :as request}]
   (layout/render request "home.html"
