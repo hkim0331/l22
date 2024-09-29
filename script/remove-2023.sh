aa4c866833dc22db10b2ae705c4f696a486e9441
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-09-30 07:19:45 +0900

    commit from m24

--- script/remove-2023.sh
+++ script/remove-2023.sh
@@ -3,5 +3,6 @@ set -o errexit -o nounset -o pipefail
 PG="psql -h localhost -U postgres"
 ${PG} l22 -c "update users set subj='python' where login='hkimura'"
 ${PG} l22 -c "delete from users where subj is null"
+${PG} l22 -c "delete from users where created_at < '2024-04-01'"
 ${PG} l22 -c "update users set updated_at=CURRENT_TIMESTAMP where login='hkimura'"
 echo done
